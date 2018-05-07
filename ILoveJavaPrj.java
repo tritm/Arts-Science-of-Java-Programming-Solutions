a6b06a51011948d834ae395b6cd2b2e1d47c1aac
Thobias-Cerqueira <juliothobias@gmail.com>
2018-05-06 21:40:20 -0300

    Ch2
    Those are the exercises for Ch.2 Note* Chapter has no programming exercises.

---
+++ ILoveJavaPrj.java
@@ -0,0 +1,22 @@
+/*File name: ILoveJavaPrj.java
+ * -------------------------------
+ * This programs tests if the library acm is working
+ * properly and is part of the exercise set on
+ * the book Arts and Science of Java by Eric S. Roberts
+ * on page 37.
+ * The Program displays I Love Java as proposed
+ * in the book.
+ */
+
+//imports 
+
+import acm.graphics.*;
+import acm.program.*;
+
+//public voids
+public class ILoveJavaPrj extends GraphicsProgram {
+	public void run () {
+		add(new GLabel("I Love Java"), 100, 75);
+	}
+	
+}
