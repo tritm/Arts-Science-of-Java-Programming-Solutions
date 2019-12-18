0cd98fbe8b495cd593ed1546b1bbb1e121a3f72b
Tri Trinh <tmtri81@gmail.com>
2019-12-18 22:36:42 +0700

    Finish writing a class testDrawRainbow to test the class DrawRainbow
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

--- Ch2/DrawRainbow.java
+++ Ch2/DrawRainbow.java
@@ -1,29 +1,25 @@
 
-/*File: DrawRainbow.java
- *This file contains the drawing of a Rainbow
- *in Java using acm graphics and program imports.
- *This is an exercise proposed by Eric S Roberts
- *According to his book Arts and Science of Java
- *in page 56 exercise: 9. @author: Tri Trinh
- */
-
-//imports
-
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import java.awt.*;
 
+/**
+ * Draw a colorful rainbow on a window. This class inheritance
+ * the class GraphicsProgram to draw on window. This class can
+ * run as standalone by edit configuration/ main class: DrawRainbow
+ * or used by other class
+ */
 public class DrawRainbow extends GraphicsProgram {
-	public void run () {
+    public void run() {
 //		index of list is from 0 to 6, index of Color.red is cls[0], use [] but not () to get an object from list
-		GOval[] strips = new GOval[7];
-		Color[] cls    = new Color[]{Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta, Color.cyan};
-		this.setBackground(Color.cyan);
-		for (int i = 0; i < 7; i++) {
-			strips[i] = new GOval((i*50 - 325), ((i+1)*50), (1400 - i*100), (1400 - i*100));
-			strips[i].setFillColor(cls[i]);
-			strips[i].setFilled(true);
-			add(strips[i]);
-		}
-	}
+        GOval[] strips = new GOval[7];
+        Color[] cls    = new Color[]{Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta, Color.cyan};
+        this.setBackground(Color.cyan);
+        for (int i = 0; i < 7; i++) {
+            strips[i] = new GOval((i*50 - 325), ((i+1)*50), (1400 - i*100), (1400 - i*100));
+            strips[i].setFillColor(cls[i]);
+            strips[i].setFilled(true);
+            add(strips[i]);
+        }
+    }
 }
---
+++ Ch2/testDrawRainbow.java
@@ -0,0 +1,9 @@
+/**
+ * test the class DrawRainbow
+ */
+public class testDrawRainbow {
+    public static void main (String[] args) {
+        DrawRainbow rb = new DrawRainbow();
+        rb.start();
+    }
+}
