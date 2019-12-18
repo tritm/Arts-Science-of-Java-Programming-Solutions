eb6ef7eb91a941b3503093a0645859d7393d2b48
Tri Trinh <tmtri81@gmail.com>
2019-12-18 16:32:36 +0700

    Finish drawing cyan sky with colorful rainbow
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

--- Ch2/DrawRainbow.java
+++ Ch2/DrawRainbow.java
@@ -1,3 +1,4 @@
+
 /*File: DrawSnowMan.java
  *This file contains the drawing of a Snowman
  *in Java using acm graphics and program imports.
@@ -8,19 +9,47 @@
 
 //imports
 
-import acm.graphics.GLabel;
-import acm.graphics.GOval;
-import acm.graphics.GRect;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
+import java.awt.*;
 
 public class DrawRainbow extends GraphicsProgram {
 	public void run () {
-		add(new GOval(-375, 50, 1500, 1500)); //  strip 0
-		add(new GOval(-325, 100, 1400, 1400)); // strip 1
-		add(new GOval(-275, 150, 1300, 1300)); // strip 2
-		add(new GOval(-225, 200, 1200, 1200)); // strip 3
-		add(new GOval(-175, 250, 1100, 1100)); // strip 4
-		add(new GOval(-125, 300, 1000, 1000)); // strip 5
-		add(new GOval(-75, 350, 900, 900)); //first arc
+		this.setBackground(Color.cyan);
+
+		GOval strip1 = new GOval(-325, 50, 1400, 1400); //strip 1
+		strip1.setFillColor(Color.red);
+		strip1.setFilled(true);
+		add(strip1);
+
+		GOval strip2 = new GOval(-275, 100, 1300, 1300); // strip 2
+		strip2.setFillColor(Color.orange);
+		strip2.setFilled(true);
+		add(strip2);
+
+		GOval strip3 = new GOval(-225, 150, 1200, 1200); // strip 3
+		strip3.setFillColor(Color.yellow);
+		strip3.setFilled(true);
+		add(strip3);
+
+		GOval strip4 = new GOval(-175, 200, 1100, 1100); // strip 4
+		strip4.setFillColor(Color.green);
+		strip4.setFilled(true);
+		add(strip4);
+
+		GOval strip5 = new GOval(-125, 250, 1000, 1000); // strip 5
+		strip5.setFillColor(Color.blue);
+		strip5.setFilled(true);
+		add(strip5);
+
+		GOval strip6 = new GOval(-75, 300, 900, 900); //first arc
+		strip6.setFillColor(Color.magenta);
+		strip6.setFilled(true);
+		add(strip6);
+
+		GOval strip7 = new GOval(-25, 350, 800, 800); //first arc
+		strip7.setFillColor(Color.cyan);
+		strip7.setFilled(true);
+		add(strip7);
 	}
 }
