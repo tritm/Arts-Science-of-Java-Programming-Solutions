b080c36642f68f615df11256d8aebe2c5ee23211
Tri Trinh <tmtri81@gmail.com>
2019-12-18 23:14:02 +0700

    Finish writing testInterface to demonstrate how to use interface and implements
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

---
+++ Ch2/testInterface.java
@@ -0,0 +1,27 @@
+/**
+ * This file demonstrates the interface and implement. We create an interface Animal
+ * with 2 method animalSound() and sleep(). Then we create a class Pig to implements Animal Interface
+ * Infact, there are many interfaces available, we can use it
+ */
+interface Animal {
+    public void animalSound(); // interface method (does not have a body)
+    public void sleep(); // interface method (does not have a body)
+}
+
+class Pig implements Animal {
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+    public void sleep() {
+        System.out.println("The pig sleep: Zzz");
+    }
+}
+
+public class testInterface {
+    public static void main(String[] args) {
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
+
