663a6935ccbba1e7711252eea966e2afe6f56434
Tri Trinh <tmtri81@gmail.com>
2019-12-28 00:58:08 +0700

    Finish re-writing TicTacToe.java
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

--- Ch3/TicTacToe.java
+++ Ch3/TicTacToe.java
@@ -2,9 +2,15 @@ import acm.graphics.*;
 import acm.program.*;
 public class TicTacToe extends GraphicsProgram {
     public void run(){
-        add(new GLine(30,60,120,60));
-        add(new GLine(30,90,120,90));
-        add(new GLine(60,30,60,120));
-        add(new GLine(90,30,90,120));
+        int w = getWidth();
+        int h = getHeight();
+        add(new GLine((w - BOARD_SIZE)/2,(h - 30)/2,(w + BOARD_SIZE)/2,(h - 30)/2));
+        add(new GLine((w - BOARD_SIZE)/2,(h + 30)/2,(w + BOARD_SIZE)/2,(h + 30)/2));
+        add(new GLine((w - 30)/2,(h - BOARD_SIZE)/2,(w - 30)/2,(h + BOARD_SIZE)/2));
+        add(new GLine((w + 30)/2,(h - BOARD_SIZE)/2,(w + 30)/2,(h + BOARD_SIZE)/2));
     }
+    private static final int BOARD_SIZE = 90;
+
+
+
 }
