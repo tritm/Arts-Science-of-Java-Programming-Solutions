e751e941c614afe62236c8b191365149338c44a8
Tri Trinh <tmtri81@gmail.com>
2020-01-01 18:21:14 +0700

    Finish AskYesNoQuestion.java
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

---
+++ Ch5/AskYesNoQuestion.java
@@ -0,0 +1,32 @@
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+
+/**
+ * Draw an archery target with 3 rings
+ */
+public class AskYesNoQuestion extends ConsoleProgram {
+
+    public void run() {
+        while (true) {
+            if (askYesNoQuestion("Would you like instructions? ")) {
+                println("Pls answer yes or no");
+            } else {
+                println("Thank you for good answer");
+                pause(1000);
+                exit();
+            }
+        }
+    }
+    private boolean askYesNoQuestion(String s) {
+        String result = readLine(s);
+        if (result.contentEquals("yes") || result.contentEquals("no")) return false;
+        else return true;
+    }
+}
+    
