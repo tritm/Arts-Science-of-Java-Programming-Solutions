185ff807526faba1f9c694bb6fe843d774675999
Tri Trinh <tmtri81@gmail.com>
2020-01-01 11:38:57 +0700

    Finish recursive FibonacciMethod
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

--- Ch5/FibonacciMethod.java
+++ Ch5/FibonacciMethod.java
@@ -1,31 +1,28 @@
-/*File: FibonacciSequence.java
+/**File: FibonacciSequence.java
  * --------------------------------
  * This program will print a Fibonacci's Sequence
  * below 10,000.
  * modified from:beginnersbook.com
  */
 
-//imports
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import acm.program.ConsoleProgram;
-public class FibonacciMethod extends ConsoleProgram{
-	//The SENTINEL to stop when desired.
-	private static final int SENTINEL = 10000;
-	public void run () {
-		println("This program will demonstrate the numbers from the Fibonacci"
-				+ " Sequence below 10,000");
-		int start = 0;
-		int sequence = 1;
-		/*This tests whatever value necessary to tell the Fibonacci Sequence to continue.
-		 * It was necessary to use a while loop instead of a for loop.*/
-		int i = 1;
-		while (start < SENTINEL) {
-			println("-> " + start + ""); //This way println starts with 0 and the F.S continues after value is looped.
-			int addValuesAbove = start + sequence;// adds the subsequent values.
-			start = sequence; // than equalizes with the previous values.
-			sequence = addValuesAbove; //generates the Fibonacci Sequence with the loop.
-			i++;
+
+public class FibonacciMethod extends ConsoleProgram {
+	Logger log = LoggerFactory.getLogger(getClass());
+	public void run() {
+		int n;
+		n = readInt("Enter the number to calculate fibonacci from 1 to n: ");
+		for (int i = 0; i < n; i++){
+			println("fibonacci " + i + " = " + fibonacci(i));
 		}
+	}
+	// Calculate the nth fibonacci as recursive method
+	private int fibonacci(int n){
+		if (n == 0) return 1;
+		if (n == 1) return 1;
+		return fibonacci(n-1) + fibonacci(n-2);
 	}
 }
 
