452cfa24df2e8038a1fce887116b698bc6b5394c
Tri Trinh <tmtri81@gmail.com>
2020-01-01 23:52:47 +0700

    Finish isPrime.java
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

---
+++ Ch5/IsPrime.java
@@ -0,0 +1,31 @@
+import acm.graphics.GLine;
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+
+/**
+ * Draw an archery target with 3 rings
+ */
+public class IsPrime extends ConsoleProgram {
+    public void run() {
+        Logger log = LoggerFactory.getLogger(getClass());
+        log.info("Width = {} Height = {}", getWidth(), getHeight());
+        int num = readInt("Enter an Integer n = ");
+        if (isPrime(num)) {
+            println("The entered number " + num + " is prime number");
+        } else {
+            println("The entered number " + num + " is not a prime number");
+        }
+    }
+    private boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
