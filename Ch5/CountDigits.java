40af18172f572db9138bf9b1e062849dde6f7a47
Tri Trinh <tmtri81@gmail.com>
2020-01-01 16:15:16 +0700

    Finish CountDigits.java
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

---
+++ Ch5/CountDigits.java
@@ -0,0 +1,25 @@
+/**File: FibonacciMethod.java
+ * --------------------------------
+ * Recursive Fibonacci
+ */
+
+import acm.program.ConsoleProgram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountDigits extends ConsoleProgram {
+	Logger log = LoggerFactory.getLogger(getClass());
+	public void run() {
+		int n;
+		n = readInt("Enter base n: ");
+		println("number of Digits of " + n + " = " + countDigits(n));
+	}
+	// Calculate the nth fibonacci as recursive method
+	private double countDigits(int n) {
+		int i = 0;
+		while (true)
+			if (n/Math.pow(10,i) >= 1 && n/Math.pow(10,i) < 10) return ++i ;
+			else i++;
+	}
+}
+
---
+++ Ch5/DigitSum.java
@@ -0,0 +1,23 @@
+/**File: FibonacciMethod.java
+ * --------------------------------
+ * Recursive Fibonacci
+ */
+
+import acm.program.ConsoleProgram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DigitSum extends ConsoleProgram {
+	Logger log = LoggerFactory.getLogger(getClass());
+	public void run() {
+		println("This program sums the digits in an integer ");
+		int n = readInt("Enter a positive integer: ");
+		int dsum = 0;
+		while (n > 0) {
+			dsum += n % 10;
+			n /= 10;
+		}
+		println("The sum of digits is " + dsum);
+	}
+}
+
