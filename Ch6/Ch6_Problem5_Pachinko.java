bc9cface767d7aecac21436aea317bbd6d00f29f
Tri Trinh <tmtri81@gmail.com>
2020-02-16 00:26:27 +0700

    Finish Ch6_Problem5_Pachinko, version 1, working version but still need to compact the code
    Signed-off-by: Tri Trinh <tmtri81@gmail.com>

---
+++ Ch6/Ch6_Problem5_Pachinko.java
@@ -0,0 +1,93 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
+
+public class Ch6_Problem5_Pachinko extends ConsoleProgram {
+    RandomGenerator rgen = new RandomGenerator();
+    private static final int BAR = 1;
+    private static final int BELL = 2;
+    private static final int PLUM = 3;
+    private static final int ORANGE = 4;
+    private static final int LEMON = 5;
+    private static final int CHERRY = 6;
+    String[] strList = {"BAR", "BELL", "PLUM", "ORANGE","LEMON", "CHERRY"};
+
+    public void run() {
+        String instr = readLine("Would you like instruction (y/n)? ");
+        if (instr.equals("y") | instr.equals("yes") ) printInstruction();
+
+        String play = readLine("You have $50. Would you like to play? ");
+        if (play.equals("n") | play.equals("no")) exit();
+
+        int M1, M2, M3;
+        int budget = 50;
+        while (budget >= 0) {
+            M1 = dice();
+            M2 = dice();
+            M3 = dice();
+//            M1 = 6; M2 = 2; M3 = 1;
+            budget = calculatePrize(budget, M1, M2, M3);
+//            pause(500);
+        }
+    }
+    private int dice() { return rgen.nextInt(BAR, CHERRY); }
+    private void printInstruction() {
+        println("BAR \t BAR \t BAR \t\t pays \t $250");
+        println("BELL \t BELL \t BELL/BAR \t\t pays \t $20");
+        println("PLUM \t PLUM \t PLUM/BAR \t\t pays \t $14");
+        println("ORANGE \t ORANGE \t ORANGE/BAR \t pays \t $10");
+        println("CHERRY \t CHERRY \t CHERRY \t\t pays \t $7");
+        println("CHERRY \t CHERRY \t - \t\t pays \t $5");
+        println("CHERRY \t - \t - \t\t pays \t $2");
+
+    }
+    private int calculatePrize(int budget, int m1, int m2, int m3){
+        if (m1 == BAR && m2 == BAR && m3 == BAR) {
+            budget += 249;
+            printResult(1, budget, m1, m2, m3);
+        }
+        else if (m1 == BELL && m2 == BELL && ((m3 == BELL) || (m3 == BAR)) ) {
+            budget += 19;
+            printResult(2, budget, m1, m2, m3);
+        }
+        else if (m1 == PLUM && m2 == PLUM && ((m3 == PLUM) || (m3 == BAR))) {
+            budget += 13;
+            printResult(3, budget, m1, m2, m3);
+        }
+        else if (m1 == ORANGE && m2 == ORANGE && ((m3 == ORANGE) || (m3 == BAR) )) {
+            budget += 9;
+            printResult(4, budget, m1, m2, m3);
+        }
+        else if (m1 == CHERRY && m2 == CHERRY && m3 == CHERRY ) {
+            budget += 6;
+            printResult(5, budget, m1, m2, m3);
+        }
+        else if (m1 == CHERRY && m2 == CHERRY && ((m3 == BAR)  || (m3 == BELL) || (m3 == PLUM) || (m3 == ORANGE) || (m3 == LEMON) || (m3 == CHERRY) ) ) {
+            budget += 4;
+            printResult(6, budget, m1, m2, m3);
+        }
+        else if (m1 == CHERRY && ((m2 == BAR)  || (m2 == BELL) || (m2 == PLUM) || (m2 == ORANGE) || (m2 == LEMON) || (m3 == CHERRY) ) && ((m3 == BAR)  || (m3 == BELL) || (m3 == PLUM) || (m3 == ORANGE) || (m3 == LEMON) || (m3 == CHERRY) ) ) {
+            budget += 1;
+            printResult(7, budget, m1, m2, m3);
+        }
+        else {
+            budget--;
+            printResult(8, budget, m1, m2, m3);
+        }
+        return budget;
+    }
+    private void printResult(int c, int budget, int m1, int m2, int m3) {
+        switch (c){
+            case 1: println("BAR \t BAR \t BAR -- You win $250 -- Your budget is " + budget); break;
+            case 2: println("BELL \t BELL \t" + strList[m3-1] + "-- You win $20 -- Your budget is $" + budget); break;
+            case 3: println("PLUM \t PLUM \t" + strList[m3-1] + " -- You win $14 -- Your budget is $" + budget); break;
+            case 4: println("ORANGE \t ORANGE \t" + strList[m3-1] + "-- You win $10 -- Your budget is $" + budget); break;
+            case 5: println("ORANGE \t ORANGE \t" + strList[m3-1] + " -- You win $7 -- Your budget is $" + budget); break;
+            case 6: println("CHERRY \t CHERRY \t" + strList[m3-1] + " -- You win $5 -- Your budget is $" + budget); break;
+            case 7: println("CHERRY \t" + strList[m2-1] + "\t" + strList[m3-1] + " -- You win $2 -- Your budget is $" + budget); break;
+            case 8: println(""+ strList[m1-1] + "\t" + strList[m2-1] + "\t" + strList[m3-1] + " -- You loose -- Your budget is $" + budget); break;
+        }
+        String play = readLine("Would you like to play? ");
+        if (play.equals("n") | play.equals("no")) exit();
+    }
+}
+
--- Ch6/Ch6_Problem5and6.java
+++ Ch6/Ch6_Problem5and6.java
@@ -13,38 +13,31 @@ import acm.graphics.*;
  * 
  * 
  * */
-
 public class Ch6_Problem5and6 extends ConsoleProgram {
-	
 	//Number Generation:
-		//random generate 2 numbers within Interval CONSTANT1 & CONSTANT2
-		//Choose greater number
-		// use ivars highNum = higher number lowNum= lower number.
-		//convert lowNum randomly into negative or positive
-	
+	//random generate 2 numbers within Interval CONSTANT1 & CONSTANT2
+	//Choose greater number
+	// use ivars highNum = higher number lowNum= lower number.
+	//convert lowNum randomly into negative or positive
 	//Formula:
-		// x = highNum + lowNum
-	
+	// x = highNum + lowNum
 	//Student enters Answer = answer;
 	//Iterate 3 times for correct answer
 	//If 3 iterations incorrect then move to next question
 	//If correct choose from 1 of 5 congratulatory messages.
 	//    using random Switch method.
-	
 	//run program 5 times.
-	
-	
-	
-	
 	//Static variables that give the range of the addition/subtraction.
 	public static final int LOW_RANGE = 1;
 	public static final int HIGH_RANGE = 20;
-	
-	
+	//rgen is a new object and it calls a method.
+	RandomGenerator rgen = new RandomGenerator();
+	//ivars.
+	private int highNum;
+	private int lowNum;
+
 	public void run() {
 		setFont("Helvetica-40");
-		
-		
 		println("Welcome to the MathSim!");
 		int i=0;
 		while (i<10) {
@@ -52,10 +45,8 @@ public class Ch6_Problem5and6 extends ConsoleProgram {
 			studentCalculation();
 			i++;
 		}
-		
-		
 	}
-	
+
 	private void studentCalculation(){
 		String symbol = "";
 		if (lowNum>0){
@@ -63,13 +54,13 @@ public class Ch6_Problem5and6 extends ConsoleProgram {
 		}
 		int answer = highNum + lowNum;
 		int studentAnswer = readInt("What is " + highNum +" " + symbol + lowNum + "? ");
-		
+
 		int c = 0;
-		
+
 		if (studentAnswer == answer) {
 			println(correctAnswer());
 		}
-		
+
 		while (studentAnswer != answer && c<3) {
 			c++;
 			studentAnswer = readInt("Incorrect - Try again: ");
@@ -79,8 +70,6 @@ public class Ch6_Problem5and6 extends ConsoleProgram {
 				println(correctAnswer());
 			}
 		}
-		
-		
 		/*if (studentAnswer != answer) {
 			int c= 0;
 			while (studentAnswer != answer && c<3) {
@@ -88,30 +77,29 @@ public class Ch6_Problem5and6 extends ConsoleProgram {
 				studentAnswer = readInt("Incorrect - Try again: ");
 			}
 		} else {
-			
+
 			println(correctAnswer());
 		} */
-		
 	}
-	
+
 	private String correctAnswer() {
 		int correct = rgen.nextInt(1,5);
 		switch (correct) {
-		case 1: return ("That's right, great job!");
-		case 2: return ("Good work, you are right!");
-		case 3: return ("Good Answer!");
-		case 4: return ("Keep up the good work!");
-		case 5: return ("Perfect answer!");
-		default: return ("Correct!");
-		
+			case 1: return ("That's right, great job!");
+			case 2: return ("Good work, you are right!");
+			case 3: return ("Good Answer!");
+			case 4: return ("Keep up the good work!");
+			case 5: return ("Perfect answer!");
+			default: return ("Correct!");
+
 		}
 	}
-	
+
 	//Method numGen
 	private void numGen() {
 		int n1 = rgen.nextInt(LOW_RANGE,HIGH_RANGE);
 		int n2 =rgen.nextInt(LOW_RANGE,HIGH_RANGE);
-		
+
 		if (n1>=n2) {
 			highNum = n1;
 			lowNum  = n2;
@@ -119,26 +107,14 @@ public class Ch6_Problem5and6 extends ConsoleProgram {
 			highNum= n2;
 			lowNum=  n1;
 		}
-		
+
 		if (subtract()) {
 			lowNum= -1 * lowNum;
 		}
-		
 	}
-	
+
 	//Predicate method subtract:
 	private boolean subtract() {
 		return rgen.nextBoolean(0.5);
 	}
-	
-	
-	//rgen is a new object and it calls a method.
-	RandomGenerator rgen = new RandomGenerator();
-	
-	//ivars.
-	private int highNum;
-	private int lowNum;
-	
-
-
 }
